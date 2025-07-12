545bb014db91f0561fdc1cb28d92d334764b8071
rdmi <info@rdmi.in>
2025-07-12 14:27:01 +0530

    d

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,33 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  
+  // Ignore ESLint errors during build
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  
+  // Ignore TypeScript errors during build
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        pathname: '/wikipedia/en/thumb/4/41/Flag_of_India.svg/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com', // For Unsplash
+        pathname: '/**', //  Be more specific if possible!
+      },
+      // Add other hosts as needed
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
