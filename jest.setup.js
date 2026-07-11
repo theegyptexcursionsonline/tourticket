a610ec107af1afe722defa7e8602c4dd245a9ea7
rdmi-stack <76124485+rdmi-stack@users.noreply.github.com>
2026-07-12 00:03:15 +0530

    test: restore web encoders in jest environment

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,9 @@
 // Learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
+import { TextDecoder, TextEncoder } from 'node:util'
+
+if (!globalThis.TextEncoder) globalThis.TextEncoder = TextEncoder
+if (!globalThis.TextDecoder) globalThis.TextDecoder = TextDecoder
 
 // Polyfill web APIs for jsdom (required by Firebase, streaming libs, etc.)
 if (!globalThis.fetch) {
